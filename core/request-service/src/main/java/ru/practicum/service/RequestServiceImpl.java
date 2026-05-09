56c7b13218e6f7e6efa7aaed981108f230fb44d5
Byswork <byswork@yandex.ru>
2026-05-10 01:29:01 +0300

    Исправление ошибок. Part 1

--- core/request-service/src/main/java/ru/practicum/service/RequestServiceImpl.java
+++ core/request-service/src/main/java/ru/practicum/service/RequestServiceImpl.java
@@ -29,7 +29,7 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class RequestServiceImpl implements RequestService {
     private final RequestRepository requestRepository;
     private final RequestMapper requestMapper;
